a151a8b4991d007a60af6cb2ff3f516f90f5b84a
1zu0 <67436621+1zu0@users.noreply.github.com>
2024-04-08 01:15:42 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+	 <div class="container pageFooter">
+			<div id="contentCurve"></div>
+			<p class="text-muted disclaimer text-center ng-binding" style="font-size: 11px">If you register a new account, you agree to Netflix's <a href="https://help.netflix.com/legal/termsofuse" target="_blank">Terms & Conditions</a> and <a href="https://help.netflix.com/legal/privacy" target="_blank">Privacy Policy</a>.</p>
+	 </div>
+</footer>
+</body>
+</html>
